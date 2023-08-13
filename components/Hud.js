1b76fe5c08cfd6ce6fddf154cf1bce9f89829b11
Daria <daria.diachkova@mail.ru>
2023-08-13 22:54:59 +0200

    clean up

--- components/Hud.js
+++ components/Hud.js
@@ -1,6 +1,6 @@
 class Hud {
   constructor() {
-    this.scoreboards = [];
+    this.scoreboards = []
   }
 
   update() {
@@ -11,44 +11,38 @@ class Hud {
 
   createElement() {
     if (this.element) {
-      this.element.remove();
-      this.scoreboards = [];
+      this.element.remove()
+      this.scoreboards = []
     }
 
-    this.element = document.createElement("div");
-    this.element.classList.add("Hud");
+    this.element = document.createElement("div")
+    this.element.classList.add("Hud")
 
-    const {playerState} = window;
+    const {playerState} = window
     playerState.lineup.forEach(key => {
-      const skill = playerState.skills[key];
+      const skill = playerState.skills[key]
       if (!skill) return 
       const scoreboard = new Combatant({
         id: key,
         ...Skills[skill.skillId],
         ...skill,
       }, null)
-      scoreboard.createElement();
-      this.scoreboards.push(scoreboard);
-      this.element.appendChild(scoreboard.hudElement);
+      scoreboard.createElement()
+      this.scoreboards.push(scoreboard)
+      this.element.appendChild(scoreboard.hudElement)
     })
-    this.update();
+    this.update()
   }
 
   init(container) {
-    this.createElement();
-    container.appendChild(this.element);
-
+    this.createElement()
+    container.appendChild(this.element)
     document.addEventListener("PlayerStateUpdated", () => {
-      this.update();
+      this.update()
     })
-
     document.addEventListener("LineupChanged", () => {
-      this.createElement();
-      container.appendChild(this.element);
+      this.createElement()
+      container.appendChild(this.element)
     })
-
   }
-
-
-
 }
--- components/PauseMenu.js
+++ components/PauseMenu.js
@@ -1,15 +1,14 @@
 class PauseMenu {
   constructor({progress, onComplete}) {
-    this.progress = progress;
-    this.onComplete = onComplete;
+    this.progress = progress
+    this.onComplete = onComplete
   }
 
   getOptions(pageKey) {
-
     if (pageKey === "root") {
       const lineupSkills = playerState.lineup.map(id => {
-        const {skillId} = playerState.skills[id];
-        const base = Skills[skillId];
+        const {skillId} = playerState.skills[id]
+        const base = Skills[skillId]
         return {
           label: base.name,
           description: base.description,
@@ -24,32 +23,30 @@ class PauseMenu {
           label: "Save",
           description: "Save your progress",
           handler: () => {
-            this.progress.save();
-            this.close();
+            this.progress.save()
+            this.close()
           }
         },
         {
           label: "Close",
           description: "Close the pause menu",
           handler: () => {
-            this.close();
+            this.close()
           }
         }
       ]
     }
-
-    //Case 2: Show the options for just one skill (by id)
     const unequipped = Object.keys(playerState.skills).filter(id => {
-      return playerState.lineup.indexOf(id) === -1;
+      return playerState.lineup.indexOf(id) === -1
     }).map(id => {
-      const {skillId} = playerState.skills[id];
-      const base = Skills[skillId];
+      const {skillId} = playerState.skills[id]
+      const base = Skills[skillId]
       return {
         label: `Swap for ${base.name}`,
         description: base.description,
         handler: () => {
-          playerState.swapLineup(pageKey, id);
-          this.keyboardMenu.setOptions( this.getOptions("root") );
+          playerState.swapLineup(pageKey, id)
+          this.keyboardMenu.setOptions( this.getOptions("root") )
         }
       }
     })
@@ -60,49 +57,49 @@ class PauseMenu {
         label: "Move to front",
         description: "Move this skill to the front of the list",
         handler: () => {
-          playerState.moveToFront(pageKey);
-          this.keyboardMenu.setOptions( this.getOptions("root") );
+          playerState.moveToFront(pageKey)
+          this.keyboardMenu.setOptions( this.getOptions("root") )
         }
       },
       {
         label: "Back",
         description: "Back to root menu",
         handler: () => {
-          this.keyboardMenu.setOptions( this.getOptions("root") );
+          this.keyboardMenu.setOptions( this.getOptions("root") )
         }
       }
-    ];
+    ]
   }
 
   createElement() {
-    this.element = document.createElement("div");
-    this.element.classList.add("PauseMenu");
-    this.element.classList.add("overlayMenu");
+    this.element = document.createElement("div")
+    this.element.classList.add("PauseMenu")
+    this.element.classList.add("overlayMenu")
     this.element.innerHTML = (`
       <h2>Pause Menu</h2>
     `)
   }
 
   close() {
-    this.esc?.unbind();
-    this.keyboardMenu.end();
-    this.element.remove();
-    this.onComplete();
+    this.esc?.unbind()
+    this.keyboardMenu.end()
+    this.element.remove()
+    this.onComplete()
   }
 
   async init(container) {
-    this.createElement();
+    this.createElement()
     this.keyboardMenu = new KeyboardMenu({
       descriptionContainer: container
     })
-    this.keyboardMenu.init(this.element);
-    this.keyboardMenu.setOptions(this.getOptions("root"));
+    this.keyboardMenu.init(this.element)
+    this.keyboardMenu.setOptions(this.getOptions("root"))
 
-    container.appendChild(this.element);
+    container.appendChild(this.element)
 
-    utils.wait(200);
+    utils.wait(200)
     this.esc = new KeyPressListener("Escape", () => {
-      this.close();
+      this.close()
     })
   }
 
